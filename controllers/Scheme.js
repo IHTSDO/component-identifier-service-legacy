7efbce26ca93df3fd325ee5a15413d074d4cd880
alopezo <alopez@termmed.com>
2015-08-10 18:46:20 -0300

    Reverting last 6 commits
    Revert "Revert "Replacing next for res.end""
    
    This reverts commit 5f3c786435b3846bb9e8f341a56689d408d9c5c1.

--- controllers/Scheme.js
+++ controllers/Scheme.js
@@ -6,7 +6,7 @@
 var security = require("./../blogic/Security");
 var scheme = require("../blogic/SchemeDataManager");
 
-function isAbleToEdit(schemeName, user){
+function isAbleToEdit(schemeName, user, res){
     var able = false;
     security.admins.forEach(function(admin){
         if (admin == user)
@@ -15,9 +15,11 @@ function isAbleToEdit(schemeName, user){
     if (!able){
         if (schemeName != "false"){
             scheme.getPermissions(schemeName, function(err, permissions) {
-                if (err)
-                    return next(err.message);
-                else{
+                if (err){
+                    res.setHeader('Content-Type', 'application/json');
+                    res.status(500);
+                    res.end(JSON.stringify({message: err.message}));
+                }else{
                     permissions.forEach(function(permission){
                         if (permission.role == "manager" && permission.username == user)
                             able = true;
@@ -35,12 +37,16 @@ module.exports.getSchemes = function getSchemes (req, res, next) {
     var token = req.swagger.params.token.value;
     security.authenticate(token, function(err, data) {
         if (err) {
-            return next(err.message);
+            res.setHeader('Content-Type', 'application/json');
+            res.status(400);
+            res.end(JSON.stringify({message: err.message}));
         }else{
             scheme.getSchemes(function(err, schemes) {
-                if (err)
-                    return next(err.message);
-                else{
+                if (err){
+                    res.setHeader('Content-Type', 'application/json');
+                    res.status(500);
+                    res.end(JSON.stringify({message: err.message}));
+                }else{
                     //CTV3ID	XUsHS
                     //SNOMEDID	R-FF43C
                     res.setHeader('Content-Type', 'application/json');
@@ -65,12 +71,16 @@ module.exports.getScheme = function getScheme (req, res, next) {
     var schemeName = req.swagger.params.schemeName.value;
     security.authenticate(token, function(err, data) {
         if (err) {
-            return next(err.message);
+            res.setHeader('Content-Type', 'application/json');
+            res.status(400);
+            res.end(JSON.stringify({message: err.message}));
         }else{
             scheme.getScheme(schemeName, function(err, schemes) {
-                if (err)
-                    return next(err.message);
-                else{
+                if (err){
+                    res.setHeader('Content-Type', 'application/json');
+                    res.status(500);
+                    res.end(JSON.stringify({message: err.message}));
+                }else{
                     res.setHeader('Content-Type', 'application/json');
                     res.end(JSON.stringify(schemes[0]));
                 }
@@ -98,11 +108,15 @@ module.exports.getSchemesForUser = function getSchemesForUser(req, res, next){
     var username = req.swagger.params.username.value;
     security.authenticate(token, function(err, data) {
         if (err) {
-            return next(err.message);
+            res.setHeader('Content-Type', 'application/json');
+            res.status(400);
+            res.end(JSON.stringify({message: err.message}));
         }else{
             scheme.getSchemesForUser(username, function (err, schemes){
                 if (err) {
-                    return next(err.message);
+                    res.setHeader('Content-Type', 'application/json');
+                    res.status(500);
+                    res.end(JSON.stringify({message: err.message}));
                 }else{
                     res.setHeader('Content-Type', 'application/json');
                     res.end(JSON.stringify(schemes));
@@ -117,14 +131,18 @@ module.exports.updateScheme = function updateScheme (req, res, next) {
     var schemeName = req.swagger.params.schemeName.value;
     var schemeSeq = req.swagger.params.schemeSeq.value;
     security.authenticate(token, function(err, data) {
-        if (err)
-            return next(err.message);
-        else{
-            if (isAbleToEdit(schemeName, data.user.name)){
+        if (err){
+            res.setHeader('Content-Type', 'application/json');
+            res.status(400);
+            res.end(JSON.stringify({message: err.message}));
+        }else{
+            if (isAbleToEdit(schemeName, data.user.name, res)){
                 scheme.editScheme(schemeName, schemeSeq,function(err) {
-                    if (err)
-                        return next(err.message);
-                    else{
+                    if (err){
+                        res.setHeader('Content-Type', 'application/json');
+                        res.status(500);
+                        res.end(JSON.stringify({message: err.message}));
+                    }else{
                         res.setHeader('Content-Type', 'application/json');
                         res.end(JSON.stringify({message: "Success"}));
                     }
@@ -140,12 +158,16 @@ module.exports.getPermissions = function getPermissions (req, res, next) {
     var schemeName = req.swagger.params.schemeName.value;
     security.authenticate(token, function(err, data) {
         if (err) {
-            return next(err.message);
+            res.setHeader('Content-Type', 'application/json');
+            res.status(400);
+            res.end(JSON.stringify({message: err.message}));
         }else{
             scheme.getPermissions(schemeName, function(err, schemes){
-                if (err)
-                    return next(err.message);
-                else{
+                if (err){
+                    res.setHeader('Content-Type', 'application/json');
+                    res.status(500);
+                    res.end(JSON.stringify({message: err.message}));
+                }else{
                     res.setHeader('Content-Type', 'application/json');
                     res.end(JSON.stringify(schemes));
                 }
@@ -161,13 +183,17 @@ module.exports.createPermission = function createPermission (req, res, next) {
     var role = req.swagger.params.role.value;
     security.authenticate(token, function(err, data) {
         if (err) {
-            return next(err.message);
+            res.setHeader('Content-Type', 'application/json');
+            res.status(400);
+            res.end(JSON.stringify({message: err.message}));
         }else{
-            if (isAbleToEdit(schemeName, data.user.name)){
+            if (isAbleToEdit(schemeName, data.user.name, res)){
                 scheme.createPermission({scheme: schemeName, username: username, role: role}, function(err){
-                    if (err)
-                        return next(err.message);
-                    else{
+                    if (err){
+                        res.setHeader('Content-Type', 'application/json');
+                        res.status(500);
+                        res.end(JSON.stringify({message: err.message}));
+                    }else{
                         res.setHeader('Content-Type', 'application/json');
                         res.end(JSON.stringify({message: "success"}));
                     }
@@ -184,13 +210,17 @@ module.exports.deletePermission = function deletePermission (req, res, next) {
     var username = req.swagger.params.username.value;
     security.authenticate(token, function(err, data) {
         if (err) {
-            return next(err.message);
+            res.setHeader('Content-Type', 'application/json');
+            res.status(400);
+            res.end(JSON.stringify({message: err.message}));
         }else{
-            if (isAbleToEdit(schemeName, data.user.name)){
+            if (isAbleToEdit(schemeName, data.user.name, res)){
                 scheme.deletePermission(schemeName, username, function(err){
-                    if (err)
-                        return next(err.message);
-                    else{
+                    if (err){
+                        res.setHeader('Content-Type', 'application/json');
+                        res.status(500);
+                        res.end(JSON.stringify({message: err.message}));
+                    }else{
                         res.setHeader('Content-Type', 'application/json');
                         res.end(JSON.stringify({message: "success"}));
                     }
