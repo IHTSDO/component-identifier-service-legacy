9b7e4d975b252a05133f224ca8e7617e60220cab
alopezo <alopez@termmed.com>
2015-08-17 10:09:21 -0300

    Adding login test example

--- test/restApi-security.js
+++ test/restApi-security.js
@@ -7,18 +7,37 @@ var security = require("../blogic/Security");
 var request = require('supertest');
 
 //request = request('http://localhost:3000/api');
-var baseUrl = "http://localhost:3000/api";
+var baseUrl = "http://107.170.101.181:3000/api";
 
-//describe('GET /users', function(){
-//    it('respond with json', function(done){
-//        request(baseUrl)
-//            .get('/users?token=ymBPALanrqy8VYIAkL1tQw00')
-//            .set('Accept', 'application/json')
-//            .expect('Content-Type', /json/)
-//            .expect(200)
-//            .end(function(err, res){
-//                if (err) return done(err);
-//                done()
-//            });
-//    })
-//});
+var token = "";
+
+describe('POST /login', function(){
+    it('respond with json key', function(done){
+        request(baseUrl)
+            .post('/login')
+            .field('username', 'alopez')
+            .field('password', 'snomed11')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res){
+                if (err) return done(err);
+                token = res.body.token;
+                done()
+            });
+    })
+});
+
+describe('GET /users', function(){
+    it('respond with json', function(done){
+        request(baseUrl)
+            .get('/users?token=' + token)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res){
+                if (err) return done(err);
+                done()
+            });
+    })
+});
