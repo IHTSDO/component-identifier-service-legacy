c22827122a8c29373da992eb3352a7f213bd449a
alopezo <8TPFdVPnehjv4d>
2016-10-07 17:41:48 -0300

    Adding validate sctid form

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ var swaggerDoc = require('./api/swagger-ids.json');
 // Initialize the Swagger middleware
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
     // Open cross site access for functioning as an API
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        next();
-    });
+    // app.use(function(req, res, next) {
+    //     res.header("Access-Control-Allow-Origin", "*");
+    //     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    //     next();
+    // });
     // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
     app.use(middleware.swaggerMetadata());
 
