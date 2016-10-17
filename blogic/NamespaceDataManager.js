82b22d399c5d871100e06adaf59bd59c9f763e57
tbertonatti <tbertonatti@termmed.com>
2016-10-17 19:08:32 -0300

    Test new group permissions

--- blogic/NamespaceDataManager.js
+++ blogic/NamespaceDataManager.js
@@ -20,6 +20,7 @@ module.exports.getNamespacesForUser = function getNamespacesForUser(username, ca
         if (err)
             throw err;
         else{
+            console.log("getNamespacesForUser");
             console.log(username);
             model.permissionsNamespace.find({username: username}, function (err, permissions){
                 if (err)
