4deca5d8555b605ac7bbc5fc5a43450390b4e845
ale-git-user <arodriguez@termmed.com>
2017-03-09 17:14:26 -0300

    Added service to keep 100000 ids pre generated in available status

--- blogic/BulkIdCreation.js
+++ blogic/BulkIdCreation.js
@@ -45,7 +45,6 @@ var conceptIdBulkCreation = function (namespace, partitionId, idsTotal, callback
                     else {
                         sql="SELECT sctid from auxConcept where modified_at=" + connection.escape(modified_at) + " limit " + idsTotal;
                         connection.query(sql, function (error, result) {
-                            connection.release();
                             var t5 = new Date().getTime();
                             console.log("Partial call to select data took: " + (t5 - t4) + " milisecs");
                             if (error) {
