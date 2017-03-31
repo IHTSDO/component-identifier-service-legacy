9e5cfbabb701a851c56cec050fdca5f6b4a5f55d
ale-git-user <arodriguez@termmed.com>
2017-03-31 15:08:19 -0300

    Local changes

--- test/restApi-regression-test-temp.js
+++ test/restApi-regression-test-temp.js
@@ -9,13 +9,15 @@ var request = require('supertest');
 var sctIdHelper=require("../utils/SctIdHelper");
 
 //var _host="localhost";
-var _host="localhost"; //DEV SERVER
+var _host="162.243.1.200"; //DEV SERVER
 var _port="3000";
 var _root="/api";
 var baseUrl = "http://" + _host + ":" + _port + _root;
 
-var username = process.env.test_username;
-var password = process.env.test_password;
+//var username = process.env.test_username;
+//var password = process.env.test_password;
+var username = "arodriguez";
+var password = "snomed11";
 var token = "";
 
 var guid = (function() {
--- test/restApi-regression-test.js
+++ test/restApi-regression-test.js
@@ -8,13 +8,15 @@ var security = require("../blogic/Security");
 var request = require('supertest');
 var sctIdHelper=require("../utils/SctIdHelper");
 
-var _host="localhost";
+var _host="162.243.1.200";
 var _port="3000";
 var _root="/api";
 var baseUrl = "http://" + _host + ":" + _port + _root;
 
-var username = process.env.test_username;
-var password = process.env.test_password;
+//var username = process.env.test_username;
+//var password = process.env.test_password;
+var username = "arodriguez";
+var password = "snomed11";
 var token = "";
 
 var guid = (function() {
