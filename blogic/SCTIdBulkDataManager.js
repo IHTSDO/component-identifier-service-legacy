108e6364e955be5cc2f7a2e6aceaa979970f1e24
Peter Groves Williams <petergwilliams@hotmail.com>
2017-12-20 16:43:33 +0000

    Add lock protection to small batch generation

--- blogic/SCTIdBulkDataManager.js
+++ blogic/SCTIdBulkDataManager.js
@@ -837,53 +837,54 @@ var generateSctidsSmallRequest=function (operation, callback) {
         } else {
             var cont = 0;
             var key = [parseInt(operation.namespace), operation.partitionId.toString()];
-
-            getPartition(key, function (err, data) {
-                if (err) {
-                    callback(err);
-                } else {
-                    if (!data) {
-                        callback("Partition not found for key:" + JSON.stringify(key));
-                    }
-                    var thisPartition = data;
-                    Sync(function () {
-                        var canContinue;
-                        for (var i = 0; i < operation.quantity; i++) {
-                            canContinue = true;
-
-                            try {
-                                operation.systemId = operation.systemIds[i];
-                                if (!operation.autoSysId) {
-                                    var sctIdRecord = getSyncSctidBySystemId.sync(null, operation.namespace, operation.systemId);
-                                    if (sctIdRecord != null) {
-                                        sctIdRecord.jobId = operation.jobId;
-                                        sctid.save.sync(null,sctIdRecord);
-                                        canContinue = false;
-                                    }
-                                }
-                                partitionLockManager.lockedOperation(key, function() {
-                                    if (canContinue) {
-                                        generateSctid.sync(null, operation, thisPartition);
-                                    }
-                                    cont++;
-                                    if (operation.quantity == cont) {
-                                        thisPartition.save(function (err) {
-                                            if (err) {
-                                                callback(err);
-                                            } else {
-                                                callback(null);
-                                            }
-                                        });
-                                    }
-                                });
-
-                            } catch (e) {
-                                console.error("generateSctidsSmallRequest error:" + e); // something went wrong
-                                callback(e);
-                            }
-                        }
-                    });
-                }
+            partitionLockManager.lockedOperation(key, function() {
+	            getPartition(key, function (err, data) {
+	                if (err) {
+	                    callback(err);
+	                } else {
+	                    if (!data) {
+	                        callback("Partition not found for key:" + JSON.stringify(key));
+	                    }
+	                    var thisPartition = data;
+	                    Sync(function () {
+	                        var canContinue;
+	                        for (var i = 0; i < operation.quantity; i++) {
+	                            canContinue = true;
+	
+	                            try {
+	                                operation.systemId = operation.systemIds[i];
+	                                if (!operation.autoSysId) {
+	                                    var sctIdRecord = getSyncSctidBySystemId.sync(null, operation.namespace, operation.systemId);
+	                                    if (sctIdRecord != null) {
+	                                        sctIdRecord.jobId = operation.jobId;
+	                                        sctid.save.sync(null,sctIdRecord);
+	                                        canContinue = false;
+	                                    }
+	                                }
+	                                partitionLockManager.lockedOperation(key, function() {
+	                                    if (canContinue) {
+	                                        generateSctid.sync(null, operation, thisPartition);
+	                                    }
+	                                    cont++;
+	                                    if (operation.quantity == cont) {
+	                                        thisPartition.save(function (err) {
+	                                            if (err) {
+	                                                callback(err);
+	                                            } else {
+	                                                callback(null);
+	                                            }
+	                                        });
+	                                    }
+	                                });
+	
+	                            } catch (e) {
+	                                console.error("generateSctidsSmallRequest error:" + e); // something went wrong
+	                                callback(e);
+	                            }
+	                        }
+	                    });
+	                }
+	            });
             });
         }
     });
